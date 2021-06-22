13c16c2a8697b0cd0ce4c0a1b8b301625ba13047
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-06-22 09:26:22 +0000

    body part of form done

--- pages/checkout.js
+++ pages/checkout.js
@@ -39,12 +39,116 @@ export default function Checkout() {
                 </ul>
             </div>
         </section>
-        <section className=" w-full flex flex-col font-normal text-xl py-20">
+        <section className=" w-full flex flex-col font-normal text-xl py-28">
             <div className="max-w-screen-2xl flex flex-col mx-auto w-full">
                 <div className="grid grid-cols-4">
                      <div className="col-span-3 flex flex-col space-y-10 mr-40">
                          <h3 className="text-2xl font-medium">  Select delivery method</h3>
-                         <div className="bg-white min-h-screen rounded-md shadow-sm border"></div>
+                         <div className="relative bg-white rounded-md shadow-sm border flex flex-col">
+                            <div className="flex flex-col text-xl">
+                                <div className="flex flex-row justify-between items-center border-b py-6 px-8">
+                                    <div className="flex flex-row space-x-6 text-indigo-600 items-center font-medium">
+                                        <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                                        <span>Home Delivery</span>
+                                    </div>
+                                    <span><svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" /></svg></span>
+                                </div>
+                                <div className="flex flex-col border-b py-10 px-28 space-y-8 text-gray-800">
+                                    <div className="grid grid-cols-2 gap-8">
+                                        <label className="flex flex-col space-y-3">
+                                            <span>First name</span>
+                                            <input type="text" className="focus:outline-none border border-gray-300 shadow-sm px-6 py-3 rounded-lg" name="firstname" />
+                                        </label>
+                                        <label className="flex flex-col space-y-3">
+                                            <span>Last name</span>
+                                            <input type="text" className="focus:outline-none border border-gray-300 shadow-sm px-6 py-3 rounded-lg" name="lastname" />
+                                        </label>
+                                    </div>
+                                    <label className="w-full flex flex-col space-y-3">
+                                        <span>Company</span>
+                                        <input type="text" className="focus:outline-none border border-gray-300 shadow-sm px-6 py-3 rounded-lg" name="company" />
+                                    </label>
+                                    <label className="w-full flex flex-col space-y-3">
+                                        <span>Address</span>
+                                        <input type="text" className="focus:outline-none border border-gray-300 shadow-sm px-6 py-3 rounded-lg" name="address" />
+                                    </label>
+                                    <label className="w-full flex flex-col space-y-3">
+                                        <span>Apartment, suite, etc.</span>
+                                        <input type="text" className="focus:outline-none border border-gray-300 shadow-sm px-6 py-3 rounded-lg" name="address" />
+                                    </label>
+                                    <div className="grid grid-cols-2 gap-8">
+                                        <label className="flex flex-col space-y-3">
+                                            <span>City</span>
+                                            <input type="text" className="focus:outline-none border border-gray-300 shadow-sm px-6 py-3 rounded-lg" name="city" />
+                                        </label>
+                                        <label className="flex flex-col space-y-3">
+                                            <span>Country</span>
+                                            <input type="text" className="focus:outline-none border border-gray-300 shadow-sm px-6 py-3 rounded-lg" name="country" />
+                                        </label>
+                                    </div>
+                                    <div className="grid grid-cols-3 gap-8 pb-4">
+                                        <label className="flex flex-col space-y-3">
+                                            <span>Province</span>
+                                            <input type="text" className="focus:outline-none border border-gray-300 shadow-sm px-6 py-3 rounded-lg" name="province" />
+                                        </label>
+                                        <label className="flex flex-col space-y-3">
+                                            <span>Postal code</span>
+                                            <input type="text" className="focus:outline-none border border-gray-300 shadow-sm px-6 py-3 rounded-lg" name="postalcode" />
+                                        </label>
+                                        <label className="flex flex-col space-y-3">
+                                            <span>Phone number</span>
+                                            <input type="text" className="focus:outline-none border border-gray-300 shadow-sm px-6 py-3 rounded-lg" name="phonenumber" />
+                                        </label>
+                                    </div>
+                                    <hr/>
+                                    <div>
+                                        <h3 className="py-6">Delivery method</h3>
+                                        <div className="grid grid-cols-2 gap-8">
+                                        <div className="border-[3px] border-indigo-500 space-y-10 rounded-lg p-6 flex flex-col shadow-sm">
+                                            <div className="flex flex-row justify-between">
+                                                <div className="flex flex-col space-y-2">
+                                                    <span>Standard shipping</span>
+                                                    <span className="text-gray-500">4-10 business days</span>
+                                                </div>
+                                                <svg className="w-8 h-8 text-indigo-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
+                                            </div>
+                                            <p className="text-indigo-600">$4.00</p>
+                                        </div>
+                                        <div className="border-[1.1px] border-gray-300 space-y-10 rounded-lg p-6 flex flex-col shadow-sm">
+                                            <div className="flex flex-row justify-between">
+                                                <div className="flex flex-col space-y-2">
+                                                    <span>Express shipping</span>
+                                                    <span className="text-gray-500">2-3 business days</span>
+                                                </div>
+                                                <svg className="w-8 h-8 opacity-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
+                                            </div>
+                                            <p className="text-indigo-600">$14.00</p>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    <div><button type="submit" className="px-8 py-3 rounded-lg bg-indigo-600 text-white text-base text-center focus:outline-none">Save details</button></div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col text-xl">
+                                <div className="flex flex-row justify-between items-center border-b py-6 px-8">
+                                    <div className="flex flex-row space-x-6 text-gray-500 f items-center font-medium">
+                                        <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                                        <span>Store Delivery</span>
+                                    </div>
+                                   
+                                        <span><svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" /></svg></span>
+                                </div>
+                            </div>
+                            <div className="flex flex-col text-xl bottom-0">
+                                <div className="flex flex-row justify-between items-center border-b py-6 px-8">
+                                    <div className="flex flex-row space-x-6 text-gray-500 f items-center font-medium">
+                                    <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                        <span>Pick-up Location</span>
+                                    </div>
+                                    <span><svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" /></svg></span>
+                                </div>
+                            </div>
+                         </div>
                      </div>   
                      <div className="col-span-1 flex flex-col -mx-12">
                          <h3 className="text-2xl font-medium pb-8"> Order summary</h3>
