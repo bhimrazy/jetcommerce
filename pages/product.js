c158548110a0e81794e5abeefcdef74d052a3a14
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-07-11 18:27:48 +0000

    footer done

--- pages/product.js
+++ pages/product.js
@@ -51,7 +51,7 @@ export default function Product() {
           content="https://jetcommerce.vercel.app/Product.png"
         />
           </Head>
-       <header className="w-full flex flex-col items-center shadow ">
+       <header className="w-full flex flex-col items-center shadow">
            <div className="bg-gray-800 py-2 w-full">
                 <div className="container mx-auto px-8 justify-center md:px-20 flex flex-row text-sm text-gray-400 font-normal sm:justify-between items-center">
                     <div className="hidden sm:block"><svg className="w-6 h-6 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" /><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd" /></svg></div>
@@ -84,10 +84,53 @@ export default function Product() {
                 </nav>
             </div>           
        </header>
-       <main className="w-full flex flex-col text-gray-900 items-center">
+       <main className="w-full flex flex-col text-gray-900 items-center min-h-screen">
        
        </main>
-      
+       <footer className=" w-full bg-white shadow-sm py-10 ">
+            <div className="container mx-auto px-8 md:px-20 flex flex-col w-full items-center divide-y pt-4 divide-gray-100 space-y-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-between w-full">
+                    <div className="flex flex-col space-y-4">
+                        <h3 className="font-medium text-lg text-gray-800">Account</h3>
+                        <ul className="flex flex-col text-gray-600 whitespace-nowrap space-y-2">
+                            <li>Manage Account</li>
+                            <li>Saved Items</li>
+                            <li>Orders</li>
+                            <li>Redeem Gift card</li>
+                        </ul>
+                    </div>
+                    <div className="flex flex-col space-y-4">
+                        <h3 className="font-medium text-lg text-gray-800">Service</h3>
+                        <ul className="flex flex-col text-gray-600 whitespace-nowrap space-y-2">
+                            <li>Shipping & Returns</li>
+                            <li>Warranty</li>
+                            <li>FAQ</li>
+                            <li>Find a store</li>
+                            <li>Get in touch</li>
+                        </ul>
+                    </div>
+                    <div className="flex flex-col space-y-4">
+                        <h3 className="font-medium text-lg text-gray-800">Company</h3>
+                        <ul className="flex flex-col text-gray-600 whitespace-nowrap space-y-2">
+                            <li>Who are we</li>
+                            <li>Press</li>
+                            <li>Careers</li>
+                            <li>Terms & Conditions</li>
+                            <li>Privacy</li>
+                        </ul>
+                    </div>
+                    <div className="flex flex-col space-y-4">
+                        <h3 className="font-medium text-lg text-gray-800">Connect</h3>
+                        <ul className="flex flex-col text-gray-600 whitespace-nowrap space-y-2">
+                            <li>Instagram</li>
+                            <li>Pinterest</li>
+                            <li>Twitter</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="flex flex-row w-full text-gray-500 items-center justify-between pt-6"><p className="hidden sm:block"><span className="pr-4">Shipping to Nepal ($NRS)</span><span className="border-l px-4">English</span></p><p>© 2021 Clothing Company, Ltd.</p></div>
+            </div>
+       </footer>
       </div>
     );
   }
