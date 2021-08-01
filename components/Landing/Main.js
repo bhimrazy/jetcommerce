28aed902e9195954fbded9dd39831415ad0ee7a7
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-08-01 09:05:08 +0000

    updated every thing

--- components/Landing/Main.js
+++ components/Landing/Main.js
@@ -5,17 +5,17 @@ export default function Main() {
         <Layout>
             <main className="font-inter">
                 <section className="bg-white flex flex-col container mx-auto px-8  lg:px-12 xl:px-20 relative overflow-hidden shadow-sm h-[650px] max-h-[650px]">
-                    <div className="pt-16 md:pt-32 space-y-8">
+                    <div className="pt-16 md:pt-32 space-y-8 z-5">
                         <div className="space-y-4">
-                            <h1 className="font-extrabold text-6xl">Summer styles <br /> are finally here</h1>
-                            <p className="max-w-lg text-xl text-gray-500">This year, our new summer collection wil shelter you from the harsh
+                            <h1 className="font-extrabold text-4xl md:text-5xl  xl:text-6xl">Summer styles <br /> are finally here</h1>
+                            <p className="max-w-sm xl:max-w-lg text-lg lg:text-xl text-gray-500">This year, our new summer collection wil shelter you from the harsh
                                 elements of a world that doesn't care if you live or die.
                             </p>
                         </div>
                         <button className="rounded-md px-8 py-3 bg-indigo-600 hover:bg-indigo-600/90 text-indigo-100 focus:outline-none">Shop Collection</button>
                     </div>
-                    <div className="hidden xl:grid grid-cols-3 absolute right-12 justify-center items-center gap-8  -mt-24">
-                        <div className="grid grid-cols-1 gap-8  z-0">
+                    <div className="grid grid-cols-3 absolute  lg:right-12 justify-center -bottom-32 sm:-bottom-60 lg:-bottom-24 -right-24 sm:right-10 max-w-sm sm:max-w-md md:max-w-md lg:max-w-lg xl:max-w-2xl  items-center gap-4 lg:gap-8 z-0">
+                        <div className="grid grid-cols-1 gap-4 md:gap-8 ">
                             <Image
                                 className="rounded-lg"
                                 src="https://images.unsplash.com/photo-1506277886164-e25aa3f4ef7f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
